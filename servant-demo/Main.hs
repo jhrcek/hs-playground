3232252dd0eb5f1295950fefaac2c1034cfadac8
Jan Hrček <honza.hrk@gmail.com>
2022-07-28 09:07:50 +0200

    Servant demo

--- servant-demo/Main.hs
+++ servant-demo/Main.hs
@@ -10,6 +10,47 @@ import Servant.API (Capture, Get, JSON, type (:>))
 import Servant.Server (Application, Server, serve)
 
 
+type HelloAPI = "hello" :> Capture "name" String :> Get '[JSON] String
+
+
+myHandler :: String -> Handler String
+myHandler name = pure $ "Hello " <> name <> "!"
+
+
+application :: Application
+application = serve (Proxy :: Proxy HelloAPI) myHandler
+
+
+main :: IO ()
+main = do
+    putStrLn "Listening on http://localhost:3000/hello/jan"
+    run 3000 application
+
+
+{-
+After running this (e.g. stack run on CLI or execute `main` in GHCi)
+run the following in terminal:
+
+curl -v http://localhost:3000/hello/jan -H 'content-type: application/json'
+*   Trying 127.0.0.1:3000...
+* Connected to localhost (127.0.0.1) port 3000 (#0)
+> GET /hello/jan HTTP/1.1
+> Host: localhost:3000
+> User-Agent: curl/7.79.1
+> Accept: */*
+> content-type: application/json
+>
+* Mark bundle as not supporting multiuse
+< HTTP/1.1 200 OK
+< Transfer-Encoding: chunked
+< Date: Thu, 28 Jul 2022 07:06:32 GMT
+< Server: Warp/3.3.18
+< Content-Type: application/json;charset=utf-8
+<
+* Connection #0 to host localhost left intact
+"Hello jan!"
+-}
+
 {-
 import Web.Scotty
 
@@ -22,47 +63,7 @@ main = scotty 3000 $
     html $ mconcat ["Scotty, ", beam, " me up!"]
 -}
 
-type HelloAPI = Capture "word" String :> Get '[JSON] String
-
-
 -- λ> :kind! MyServer
 -- MyServer :: *
 -- = Int -> Handler String
 type MyServer = Server HelloAPI
-
-
-myHandler :: String -> Handler String
-myHandler word = pure $ "Scotty " <> word <> " me up!"
-
-
-main :: IO ()
-main =
-    run 3000 application
-  where
-    application :: Application
-    application = serve (Proxy :: Proxy HelloAPI) myHandler
-
-{-
-After running this (e.g. stack run on CLI or execute `main` in GHCi)
-run the following in terminal:
-
-$ curl -v http://localhost:3000/beam -H 'Accept: application/json'
-*   Trying ::1:3000...
-* connect to ::1 port 3000 failed: Connection refused
-*   Trying 127.0.0.1:3000...
-* Connected to localhost (127.0.0.1) port 3000 (#0)
-> GET /beam HTTP/1.1
-> Host: localhost:3000
-> User-Agent: curl/7.69.1
-> Accept: application/json
->
-* Mark bundle as not supporting multiuse
-< HTTP/1.1 200 OK
-< Transfer-Encoding: chunked
-< Date: Thu, 08 Oct 2020 04:36:08 GMT
-< Server: Warp/3.3.13
-< Content-Type: application/json;charset=utf-8
-<
-* Connection #0 to host localhost left intact
-"Scotty beam me up!"
--}
